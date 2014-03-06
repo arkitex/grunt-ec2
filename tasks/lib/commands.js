f4f95eca00eda09fe129b51076b42394dcb192cf
Ethan Lo <arkitex@gmail.com>
2014-03-06 19:23:30 +0100

    Revised commands.js.

--- tasks/lib/commands.js
+++ tasks/lib/commands.js
@@ -4,24 +4,23 @@ var _ = require('lodash');
 var util = require('util');
 var conf = require('./conf.js');
 var parse = require('./parse.js');
-var running = '[[ $(pm2 list) != "[]" ]]';
 
 module.exports = {
-    pm2_reload: function () {
-        return util.format('%s && sudo pm2 reload all || echo "pm2 not started."', running);
-    },
-    pm2_start: function (name) {
-        var defaults = {
-            NODE_ENV: name
-        };
-        var user = conf('ENV');
-        var env = {};
+  pm2_reload: function () {
+    return util.format('sudo pm2 reload all || echo "pm2 not started."');
+  },
+  pm2_start: function (name) {
+    var defaults = {
+      NODE_ENV: name
+    };
+    var user = conf('ENV');
+    var env = {};
 
-        // user can override NODE_ENV if need be
-        _.assign(env, defaults, user);
+    // user can override NODE_ENV if need be
+    _.assign(env, defaults, user);
 
-        return util.format('%s || sudo %s pm2 start %s/%s -i 2 --name %s || echo "pm2 already started."',
-            running, 'NODE_ENV=' + env.NODE_ENV, conf('SRV_CURRENT'), conf('NODE_SCRIPT'), name
-        );
-    }
+    return util.format('pm2 kill && cd /srv/apps/ec2/current/dist/ && sudo %s pm2 start %s --name %s -f || echo "pm2 already started."',
+      'NODE_ENV=' + env.NODE_ENV, conf('NODE_SCRIPT'), name
+    );
+  }
 };
